0616ac98919b912a462bf0a9dee1d48771bb816b
Aastha <asthaagrawal46@gmail.com>
2022-10-19 11:14:26 +0530

    Challenge by AngellaYu

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App.jsx';
-// import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App.jsx';
+// // import reportWebVitals from './reportWebVitals';
+// import { BrowserRouter } from "react-router-dom";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-    ReactDOM.render(
+// // const root = ReactDOM.createRoot(document.getElementById('root'));
+//     ReactDOM.render(
 
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-,document.getElementById('root')
-    );
+//         <BrowserRouter>
+//           <App />
+//         </BrowserRouter>
+//     ,document.getElementById("root")
+//     );
+
+const Name = "Aastha";
+const currentdate = new Date();
+const year = currentdate.getFullYear();
     
+ReactDOM.render(
+  <div>
+    <h1>My name is {Name}</h1>
+    <p>Current year is {year}</p>
+  </div>,
+  document.getElementById("root")
+);
