488f62f5b1d3293b9f89b4f58d6cd69b6d319d8f
Aastha <asthaagrawal46@gmail.com>
2022-10-19 15:11:53 +0530

    Calculator using components

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,14 +1,23 @@
  import React from 'react';
  import ReactDOM from 'react-dom';
+ import {Add, Multiply, Subtract, Divide} from './components/calculator.jsx'
 // import './index.css';
- import App from './App.jsx';
+//  import App from './App.jsx';
 // // import reportWebVitals from './reportWebVitals';
 // import { BrowserRouter } from "react-router-dom";
 
 // // const root = ReactDOM.createRoot(document.getElementById('root'));
    ReactDOM.render(
-      <App /> ,document.getElementById("root")
-     );
+    <ul>
+      <li>{Add(1, 2)}</li>
+      <li>{Multiply(3, 2)}</li>
+      <li>{Subtract(6, 2)}</li>
+      <li>{Divide(6, 2)}</li>
+    </ul>,
+    document.getElementById("root")
+   // <App /> ,document.getElementById("root")
+
+   );
 
 
    
