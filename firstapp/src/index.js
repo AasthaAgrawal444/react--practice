4389e62aabce9f8dd2c2f5f3d6d3ecb8133f7b6e
Aastha <asthaagrawal46@gmail.com>
2022-10-19 12:24:09 +0530

    2nd Challenge

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -14,19 +14,40 @@
 //     ,document.getElementById("root")
 //     );
 
- const Name = "Aastha";
- const currentdate = new Date();
- const year = currentdate.getFullYear();
+//  const Name = "Aastha";
+//  const currentdate = new Date();
+//  const year = currentdate.getFullYear();
    
- const customStyle = {
-  color: "red",
-  border: "1px solid black",
- }
+  const customStyle = {
+    color : " "
+//   color: "red",
+//   border: "1px solid black",
+  }
+
+const date = new Date();
+const currentTime = date.getHours();
+
+let greeting;
+
+if(currentTime < 12){
+ greeting = "Good Morning!";
+ customStyle.color = "red";
+}else if(currentTime<18){
+ greeting = "Good Afternoon!";
+ customStyle.color = "green";
+
+}else{
+  greeting = "Good Night!"
+  customStyle.color = "blue";
+
+}
 
  ReactDOM.render(
    <div>
-     <h1 style = {customStyle}>My name is {Name}</h1>
+    {/* <h1 style = {customStyle}>My name is {Name}</h1>
      <p style= {{color: "blue"}}>Current year is {year}</p>
+   */}
+   <h1 className='heading' style={customStyle}>{greeting}</h1>
    </div>,
    document.getElementById("root")
  );
